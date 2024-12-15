9b26b0aa6cfff3cf84e34e743c10f3e14be6561c
Ankit Raj <147710734+ankit-raj00@users.noreply.github.com>
2024-12-15 22:52:03 +0530

    Update LogoutBtn2.jsx

--- src/Components/LogoutBtn2.jsx
+++ src/Components/LogoutBtn2.jsx
@@ -17,7 +17,7 @@ function LogoutBtn2() {
 
   return (
     <button
-      className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+      className='text-white inline-block px-6 py-2 rounded-lg font-medium transition-colors duration-300 hover:bg-blue-100 hover:text-blue-600 hover:underline'
       onClick={logoutHandler}
     >
       Logout

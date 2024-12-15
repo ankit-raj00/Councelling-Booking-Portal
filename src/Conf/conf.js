7d252c02fa359f8b84e34c9fc376da3fe75a1f33
Ankit Raj <147710734+ankit-raj00@users.noreply.github.com>
2024-12-15 21:08:07 +0530

    Update conf.js
    bucket id deleted

--- src/Conf/conf.js
+++ src/Conf/conf.js
@@ -5,7 +5,7 @@ const conf = {
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteCollectionId_2: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID_2),
 
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    
 }
 // there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
